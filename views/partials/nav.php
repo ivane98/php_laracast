64fbcefc87aa3ba7ea6c13275e19aa42939a3a1e
ivane98 <i.vardoshvili22@gmail.com>
2025-05-19 10:58:32 +0400

    mysql

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -12,13 +12,13 @@
                             Home
                         </a>
 
-                        <a href="/about.php"
-                            class="<?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">
+                        <a href="/about"
+                            class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">
                             About
                         </a>
 
-                        <a href="/contact.php"
-                            class="<?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">
+                        <a href="/contact"
+                            class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">
                             Contact
                         </a>
 
