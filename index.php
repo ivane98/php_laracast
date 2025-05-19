da4f0e9774d46387ba7b66968d27ddd11357d138
ivane98 <i.vardoshvili22@gmail.com>
2025-05-19 16:58:32 +0400

    DB

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
+// require 'router.php';
 
-require 'router.php';
+
+
+$db = new Database();
+
+$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
